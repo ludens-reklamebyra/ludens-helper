fd72d0c4688f6988c242f2222b8af979f9a84217
oyvindhermansen <oyvind@ludensreklame.no>
2016-06-21 15:37:15 +0200

    first test helper method is up

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ludens-helper
+
+## Install
+```
+$ git clone https://github.com/ludens-reklamebyra/ludens-helper.git
+$ npm install
+```
+## Build
+```
+$ npm run build
+```
+## Tests
+Uses mocha with Chai to test with.
+```
+$ npm test
+```
+## API-reference
+``` javascript
+// Coming soon
+```
--- src/ludenshelper.js
+++ src/ludenshelper.js
@@ -1,9 +1,30 @@
 'use strict'
+import $ from 'jquery'
 
 class Ludenshelper {
-  constructor() {
+  constructor(elem) {
+    this.elem = elem
+    this.selector = $(this.elem)
+  }
 
+  toggleOnEvent(opts) {
+    if (typeof opts != 'object') throw new Error(`Expected options to be an object`)
+    opts = ({
+      event: 'click',
+      menuElement: null,
+      toggleType: null,
+      speed: 500
+    }, opts)
+
+    if (typeof opts.toggleType != 'string') {
+      throw new Error('Expected toggleType to be a string, and to match a jquery event method')
+    }
+
+    this.selector.on(opts.event, function(e) {
+      $(opts.menuElement)[opts.toggleType](opts.speed)
+    })
+    return this
   }
 }
 
-export default new Ludenshelper()
+export default ludens = selector => new Ludenshelper(selector)
