2ca9ce60db61f3c568023678a97f81e59a2c1b37
oyvindhermansen <oyvind@ludensreklame.no>
2016-06-28 21:37:13 +0200

    dataType can now be defined

--- src/helpers/ludensajax/ludensajax.js
+++ src/helpers/ludensajax/ludensajax.js
@@ -9,13 +9,14 @@ const ludensAjax = (opts) => {
   if (!opts) {
     throw new Error(`loadmore requires an object as a param`)
   } else if (typeof opts != 'object') {
-    throw new Error(`Expected ${opts} to be an object`)
+    throw new Error(`Expected '${opts}' to be an object`)
   }
 
   opts = ({
     method: null,
     url: null,
     data: null,
+    dataType: null,
     outputTarget: null,
     cb: Function,
     loading: {
@@ -23,6 +24,7 @@ const ludensAjax = (opts) => {
       loadingText: null
     },
     errorMsg: null,
+    jsonResult: Function
   }, opts)
 
   const setup = {} || null
@@ -30,6 +32,7 @@ const ludensAjax = (opts) => {
   setup.type = opts.method
   setup.url = opts.url
   setup.data = opts.data ? setup.data = opts.data : null
+  setup.dataType = opts.dataType ? setup.dataType = opts.dataType : null
 
   setup.beforeSend = () => {
     if (opts.loading) {
@@ -39,14 +42,24 @@ const ludensAjax = (opts) => {
 
   setup.success = (data) => {
     if (!data) throw new Error('There is no data to collect')
-    $(opts.outputTarget).append(data)
+    let lowerCasedDataType = setup.dataType ? setup.dataType.toLowerCase() : null
+    if (lowerCasedDataType === 'json') {
+      if (typeof opts.jsonResult === 'function') {
+        opts.jsonResult(data)
+      } else {
+        throw new Error(`Expected '${opts.jsonResult}' to be a function`)
+      }
+    } else {
+      $(opts.outputTarget).append(data)
+    }
+    $(opts.loading.selector).remove()
   }
 
   setup.complete = () => {
     if (opts.cb && typeof opts.cb == 'function') {
       opts.cb()
     } else {
-      throw new Error(`Expected ${opts.cb} to be a function`)
+      throw new Error(`Expected '${opts.cb}' to be a function`)
     }
   }
 
